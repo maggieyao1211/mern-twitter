5840fa22502dcfdea95d7540433b33565a29da30
maggieyao1211 <momokoyao@hotmail.com>
2020-08-31 17:42:10 -0700

    mern-twitter commit

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
-// mongodb+srv://admin:<password>@mern.jpugl.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Conneced to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.com",
+        password: "jimisgreat123",
+    });
+    user.save();
     res.send("Hello World");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
